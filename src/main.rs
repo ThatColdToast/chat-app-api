c84dc83d09504aa57e6646c85a732c747ac1ecef
ThatColdToast <thatcoldtoast@gmail.com>
2023-12-19 00:48:43 -0600

    move index.html & post_message broken

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,93 @@
-use std::time::Instant;
-
-use axum::{routing::get, Router};
+use axum::{
+    extract::{Path, Query, Request, State},
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use serde::Serialize;
+use std::{collections::HashMap, sync::Arc, time::Instant};
 use tinytemplate::TinyTemplate;
+use url::Url;
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone, Debug)]
 struct Chat {
     name: String,
     body: String,
 }
 
+#[derive(Serialize, Clone, Debug)]
+struct AppState {
+    chats: Vec<Chat>,
+}
+
 #[tokio::main]
 async fn main() {
-    // Create a new Axum router
+    // let pool = SqlitePool::connect("sqlite::memory:").await.unwrap();
+    // let mut chats: Vec<Chat> = Vec::new();
+
+    let appState = AppState { chats: Vec::new() };
+
     let app = Router::new()
-        // Define a route for the root path that responds with "Hello, World!"
-        .route("/api/chat", get(get_chat).post(post_chat));
+        .route("/", get(root))
+        .route("/api/chat", get(get_messages).post(post_message))
+        .with_state(appState);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_chat() -> String {
+async fn root() -> Html<&'static str> {
+    Html(include_str!("html/index.html"))
+}
+
+async fn get_messages(State(appState): State<AppState>) -> Html<String> {
     let start = Instant::now();
 
-    let chat_template = include_str!("chat.html");
+    println!("{:?}", appState.chats.len());
+
+    let chat = match appState.chats.last() {
+        Some(c) => c,
+        None => return Html("No messages".to_string()),
+    };
+
+    let chat_template = include_str!("html/chat.html");
     let mut tt = TinyTemplate::new();
     tt.add_template("chat", chat_template).unwrap();
 
-    let chat = Chat {
-        name: "Username".to_string(),
-        body: "Message 1".to_string(),
-    };
-
-    let result = tt.render("chat", &chat).unwrap();
+    let result = tt.render("chat", chat).unwrap();
 
     let duration = start.elapsed();
     println!("{:?}", duration);
-    result
+
+    Html(result)
 }
 
-async fn post_chat() -> String {
-    let start = Instant::now();
+async fn post_message(
+    State(mut appState): State<AppState>,
+    req: String,
+    // Form(chatbox): Form<String>,
+    // Query(params): Query<HashMap<String, String>>,
+) -> Html<String> {
+    println!("{:?}", req);
+    let req = match urlencoding::decode(&req) {
+        Ok(r) => r
+            .to_string()
+            .chars()
+            .skip("message-box=".len())
+            .collect::<String>(),
+        Err(e) => {
+            println!("{:?}", e);
+            return Html("Error".to_string());
+        }
+    };
+    println! {"{:?}", req};
 
     let chat = Chat {
         name: "Username".to_string(),
-        body: "Message 1".to_string(),
+        body: req,
     };
 
-    let duration = start.elapsed();
-    println!("{:?}", duration);
-    "Success".to_string()
+    appState.chats.push(chat);
+
+    Html("Success".to_string())
 }
-
-// async fn get_chats() -> &'static str {
-//     let chat_template = include_str!("chat.html");
-//     let mut tt = TinyTemplate::new();
-//     tt.add_template("chat", chat_template);
-
-//     let chat = Chat {
-//         name: "Username".to_string(),
-//         body: "Message".to_string(),
-//     };
-
-//     tt.render("hello", &chat)
-// }
